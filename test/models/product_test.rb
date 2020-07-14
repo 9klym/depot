617c40f15ce1b3b223350147664407918c3660a5
Alexander Klymenko <alexanderklymenko55@gmail.com>
2020-07-14 15:42:19 +0300

    Task B done, added fixture

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -54,7 +56,16 @@ class ProductTest < ActiveSupport::TestCase
     end
 
     bad.each do |image_url|
-      assert new_product(image_url).invalid? , "'#{image_url}', shouldn't be valid"
+      assert new_product(image_url).invalid?, "'#{image_url}', shouldn't be valid"
     end
+  end
+
+  test "product is not valid without unique title" do
+  product = Product.new(title: products(:ruby).title,
+                        description: "yyy",
+                        price: 1,
+                        image_url: "fred.gif")
+  assert product.invalid?
+  assert_equal ["has already been taken"], product.errors[:title]
   end
 end

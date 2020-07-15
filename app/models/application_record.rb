ccddbaa74dae76a0cf529371bdc04ec698d5b30f
Alexander Klymenko <alexanderklymenko55@gmail.com>
2020-07-16 01:22:38 +0300

    Additional task finished: added counter of queries to index store page

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+end
+
+def count_of_queries_to_main_store_index
+  if session[:counter].to_i > 5
+    return "Count of queries to main store page index = #{session[:counter]}"
+  else
+    return
+  end
 end

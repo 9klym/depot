cf6ca1cd2a9be76d02b36b85613c72718b3627a8
Alexander Klymenko <alexanderklymenko55@gmail.com>
2020-07-15 13:07:53 +0300

    Task D: created connection products to cart and tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,7 +23,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   # test "should create product" do
   #   assert_difference('Product.count') do
-  #     post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+  #     post products_url, params: { product: { description: @product.description, image_url: @product.image_url,
+  #                                             price: @product.price, title: @product.title } }
   #   end
   #
   #   assert_redirected_to product_url(Product.last)
@@ -40,15 +41,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: {product: {description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title}}
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:crystal))
     end
-
     assert_redirected_to products_url
   end
+
+  # test "should destroy product" do
+  #   assert_difference('Product.count', -1) do
+  #     delete product_url(@product)
+  #   end
+  #   assert_redirected_to products_url
+  # end
+
 end
